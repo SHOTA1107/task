c05f483dacb1534c0331ad9dcc54d0158977d1a3
SHOTA1107 <s.kobayashi11788@gmail.com>
2019-05-11 14:51:16 +0900

    new create
    新規投稿と作成
    ※リセットCSSを入れるとbootstrapが崩れるためインポートしていない

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,8 +6,20 @@ class TodosController < ApplicationController
   end
 
   def new
+    @todo = Todo.new
   end
 
   def edit
+  end
+
+  def create
+    todo = Todo.new(todo_params)
+    todo.save!
+    redirect_to todos_url, notice: "Todo #{todo.name} を登録しました"
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:name, :description)
   end
 end
