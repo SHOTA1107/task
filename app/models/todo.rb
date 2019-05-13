8a4c7930d6669b54475071f50f6b72184b34d61d
SHOTA1107 <s.kobayashi11788@gmail.com>
2019-05-13 14:31:13 +0900

    user-controller

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,9 @@
 class Todo < ApplicationRecord
+  validates :name, presence: true
+  validate :validate_name_not_including_comma
+
+  private
+  def validate_name_not_including_comma
+    errors.add(:name, 'カンマを含めることはできません') if name&.include?(',')
+  end
 end
