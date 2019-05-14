3308dd5a11f3fd81872158f8c9e8fd92318c99e8
SHOTA1107 <s.kobayashi11788@gmail.com>
2019-05-14 14:24:31 +0900

    user-admin
    管理者機能実装
    ※管理者のID・PASSはコンソールから作成

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,6 +2,8 @@ class Todo < ApplicationRecord
   validates :name, presence: true
   validate :validate_name_not_including_comma
 
+  belongs_to :user
+
   private
   def validate_name_not_including_comma
     errors.add(:name, 'カンマを含めることはできません') if name&.include?(',')
